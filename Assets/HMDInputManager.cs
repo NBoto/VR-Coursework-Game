75373905df70999b13aadbc67cb33631a47f7378
NBoto <n.petrie2@rgu.ac.uk>
2024-02-06 12:43:38 +0000

    So had to do a github repos on the website. I hate git.

---
+++ Assets/HMDInputManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HMDInputManager : MonoBehaviour
+{
+    [SerializeField] GameObject VRRig;
+    [SerializeField] GameObject FPSRig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (XRSettings.isDeviceActive || XRSettings.loadedDeviceName == "OpenXR Display")
+        {
+            Debug.Log("Using Device: " + XRSettings.loadedDeviceName);
+            FPSRig.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Using FPS Rig");
+            VRRig.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
